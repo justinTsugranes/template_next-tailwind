16b202a4773c9b9675db1ade115406d99fa946f9
justintsugranes <justintsugranes@proton.me>
2022-08-09 14:33:37 -0600

    added repo button

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Footer from '../components/Footer';
 import Layout from '../layouts/Layout'
 import Header from '../components/Header';
+import Link from 'next/link'
 
 export default function Index () {
   return (
@@ -13,10 +14,13 @@ export default function Index () {
         
         <main>
           <div className='flex items-center justify-center'>
-
             <h1 className='text-4xl font-bold text-blue-600 flex items-center justify-center h-1/2 min-h-screen'>
-              Hi! I'm Justin. Welcome to my Next.js Boilerplate.</h1>
-            
+              Hi! I'm Justin. Welcome to my Next.js Boilerplate</h1>
+            <div className="flex items-center justify-center">
+              <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow flex items-center justify-center">
+                <Link href="https://github.com/justinTsugranes/boilerplate_next-tailwind">GitHub Repo</Link>
+              </button>
+            </div>
           </div>
         </main>
 
